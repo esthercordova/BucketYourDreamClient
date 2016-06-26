81385622dc225e8a271a6c86870b89b98b951bf3
esthercordova <estherpiacordova@gmail.com>
2016-06-25 21:22:09 -0400

    Able to get data from input fields in console

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -37,12 +37,23 @@ const onChangePassword = (event) => {
   .fail(ui.passwordFailure);
 };
 
+const onCreateItem = (event) => {
+  event.preventDefault();
+  let dueDate = $('#todo-form :input[name=due-date]').val();
+  let dreamDescription = $('#todo-form :input[name=dream-description]').val();
+  let title = $('#todo-form :input[name=title]').val();
+  console.log("dueDate = " + dueDate + "\ntitle = " + title + "\ndescription = "+ dreamDescription) ;
+  console.log(event.target);
+
+};
+
 
 const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp);
   $('#sign-in').on('submit', onSignIn);
   $('#sign-out').on('submit', onSignOut);
   $('#change-password').on('submit', onChangePassword);
+  $('#todo-form').on('submit', onCreateItem);
 };
 //
 module.exports = {
